f4a78610e76f92be199a7b3d2c55f52835c3528a
ChangKe123 <162865233+ChangKe123@users.noreply.github.com>
2024-11-11 23:06:34 +0800

    Update Codes

---
+++ datasets/sarcasm/to_train.sh
@@ -0,0 +1,6 @@
+mv train.with_graph.pkl train.txt.test
+mv valid.with_graph.pkl valid.txt.test
+mv test.with_graph.pkl test.txt.test
+mv train.with_graph.pkl.bak train.with_graph.pkl
+mv valid.with_graph.pkl.bak valid.with_graph.pkl
+mv test.with_graph.pkl.bak test.with_graph.pkl
