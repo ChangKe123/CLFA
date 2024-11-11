ede2b756a90e2403df651af70a9d24a87898d64a
ChangKe123 <162865233+ChangKe123@users.noreply.github.com>
2024-11-11 15:01:06 +0000

    Upload Codes

---
+++ datasets/sarcasm/to_test.sh
@@ -0,0 +1,7 @@
+mv train.with_graph.pkl train.with_graph.pkl.bak
+mv valid.with_graph.pkl valid.with_graph.pkl.bak
+mv test.with_graph.pkl test.with_graph.pkl.bak
+mv train.txt.test train.with_graph.pkl
+mv valid.txt.test valid.with_graph.pkl
+mv test.txt.test test.with_graph.pkl
+
